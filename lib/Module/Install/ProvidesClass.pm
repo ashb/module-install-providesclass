6f7113e96b9c422afd578090daa6b7ed952625e8
Ash Berlin <ash_github@firemirror.com>
2010-03-05 17:38:33 +0000

    Version 1.000000

--- lib/Module/Install/ProvidesClass.pm
+++ lib/Module/Install/ProvidesClass.pm
@@ -8,7 +8,7 @@ use Module::Install::Base;
 BEGIN {
   our @ISA = qw(Module::Install::Base);
   our $ISCORE  = 1;
-  our $VERSION = '0.000001_99';
+  our $VERSION = '1.000000';
 }
 
 sub _get_no_index {
@@ -75,25 +75,48 @@ Module::Install::ProvidesClass - provides detection in META.yml for 'class' keyw
 
 This class is designed to populate the C<provides> field of META.yml files so
 that the CPAN indexer will pay attention to the existance of your classes,
-rather than blithely ignoring them.
+rather than blithely ignoring them. It does this for Module::Install based
+C<Makefile.PL> files. If you use Module::Build then look at L<Class::Discover>
+which does all the effort of searching for the classes.
+
+=head1 USAGE.
+
+Simply add the following lines to your Makefile.PL before the C<WriteAll;>:
+
+ auto_provides_class;
+
+Its that simple. By default we look for 'class' and 'role' keywords. If you are
+using something that provides packages using a different keyword, such as
+L<CatalystX::Declare> then you can pass a list of keywords to look for to
+C<auto_provides_class>:
+
+ auto_provides_class(qw/
+   class
+   role
+   application
+   controller
+   controller_role
+   view
+   model
+ /);
+
+Make sure you include 'class' and 'role' if you are still using them.
 
 The version parsing is basically the same as what M::I's C<< ->version_form >>
 does, so should hopefully work as well as it does.
 
-Currently we only support 'class' as the keyword to look for. This will
-certainly need changing to be configurable since MooseX::Declare allows C<role>
-as a keyword to create role classes.
-
-This module attempts to be author side only, hopefully it does it correctly, bu
+This module attempts to be author side only, hopefully it does it correctly, but
 Module::Install is scary at times.
 
 =head1 SEE ALSO
 
 L<MooseX::Declare> for the main reason for this module to exist.
 
+L<Class::Discover> for the version extraction logic.
+
 =head1 AUTHOR
 
-Ash Berlin C<< <ash@cpan.org> >>
+Copyright (C) Ash Berlin C<< <ash@cpan.org> >>, 2009-2010.
 
 =head1 LICENSE 
 
